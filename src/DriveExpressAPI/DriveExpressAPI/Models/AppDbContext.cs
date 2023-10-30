2a3f99ca00c6dcb8f8f9544bbb0a32004ed370d2
Matheus Fernandes <mfernandesr97@gmail.com>
2023-10-29 22:41:11 -0300

    api matheus

---
+++ src/DriveExpressAPI/DriveExpressAPI/Models/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DriveExpressAPI.Models
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions options) : base (options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<RestauranteUsuarios>()
+                .HasKey(c => new { c.RestauranteId, c.UsuarioId });
+
+            builder.Entity<RestauranteUsuarios>()
+                .HasOne(c => c.Usuario).WithMany(c => c.Restaurantes)
+                .HasForeignKey(c => c.UsuarioId);
+        }
+
+        public DbSet<Restaurante> Restaurantes { get; set; }
+
+        public DbSet<Cardapio> Cardapios { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+
+        public DbSet<RestauranteUsuarios> RestauranteUsuarios { get; set; }
+    }
+}
---
+++ src/DriveExpressAPI/DriveExpressAPI/Models/Restaurante.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DriveExpressAPI.Models
+{
+    [Table("Restaurantes")]
+    public class Restaurante : LinksHATEOS
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Endereco { get; set; }
+
+        [Required]
+        public string Telefone { get; set; }
+
+        [Required]
+        public Categoria Categoria { get; set; }
+
+        public ICollection <Cardapio> Cardapios { get; set; }
+    }
+
+    public enum Categoria
+    {
+        Brasileira,
+        Japonesa,
+        FastFood
+    }
+}
